970f96c1d76e4be2117db72a1f09cec5eb83637c
edwin ismail <typhoon_ed@yahoo.com>
2017-12-04 13:04:19 +0300

    general modification

--- port/src/components/Item.js
+++ port/src/components/Item.js
@@ -1,8 +1,21 @@
 import React from "react";
 
 const Item = props => {
+  const webLink = props.workItem.webLink ? (
+    <p>
+      Please click{" "}
+      <a href={props.workItem.webLink}>
+        <b>here</b>
+      </a>{" "}
+      to view the company website.
+    </p>
+  ) : null;
   return (
     <div className="w3-container">
+      <h4 className="w3-text-red">
+        <b>{props.workItem.company}</b>
+      </h4>
+      <h4 className="w3-text-black"> {webLink}</h4>
       <h5 className="w3-opacity">
         <b>{props.workItem.position}</b>
       </h5>
